a877bb46ec28bb5c774b7dac191cc9c1e4e6c3fb
Gustavo01833 <gustavo.opereira@sptech.school>
2025-05-16 16:08:04 -0300

    Alteração da Modelagem Lógica

--- Banco de Dados/Script - Mitos do Multiverso.sql
+++ Banco de Dados/Script - Mitos do Multiverso.sql
@@ -2,6 +2,9 @@ CREATE DATABASE mitosMultiverso;
 USE mitosMultiverso;
 
 CREATE TABLE usuario(
-	idUsuario int primary key auto_increment
-
+	idUsuario int primary key auto_increment,
+	nome varchar(45),
+    senha varchar(45),
+    email varchar(255),
+    dataCriacao date default timestamp
 );
