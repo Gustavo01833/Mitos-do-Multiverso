229228c3c3e5d727407c3b3de3f5ae84deb66d42
Gustavo01833 <gustavo.opereira@sptech.school>
2025-05-17 22:32:41 -0300

    Adição dos Selects de listagem de sala

--- Banco de Dados/Script - Mitos do Multiverso.sql
+++ Banco de Dados/Script - Mitos do Multiverso.sql
@@ -43,7 +43,7 @@ CREATE TABLE participacao(
 );
 
 CREATE TABLE personagem(
-	idPersonagem int primary key,
+	idPersonagem int primary key auto_increment,
     nome varchar(45) not null,
     vida int null,
     fkSala int,
@@ -78,15 +78,155 @@ INSERT INTO sala (fkDono, fkSistema, nome, frequencia, visibilidade, descricao, 
 SELECT * FROM sala;    
 
 INSERT INTO participacao(pkSala, pkUsuario) VALUES
+	(1,1),
 	(1,2),
     (1,3),
-	(2,1),
-	(2,3);
+    
+	(2,2),
+	(2,3),
 
+	(3,3);
+    
 select * from participacao;
 
 
-INSERT INTO personagem (nome,vida
 
+INSERT INTO personagem (nome,vida, fkSala,fkSistema,fkUsuario) VALUES
+	('Carmoleão', 10, 1, 1, 1),
+	('Adalberto', 14, 1 ,1, 2),
+	('Geralt', 13 , 1, 1, 3),
+    
+    ('Queiroz', 9 , 2, 1, 1),
+	('Vilhena', 13 , 2, 1, 1);
 
+SELECT * FROM personagem;
 
+SELECT * FROM sala;
+
+BEGIN; -- Listagem de Salas
+
+	-- LISTAGEM DE SALAS NO GERAL
+SELECT concat((select count(par.pkUsuario) FROM 
+             participacao JOIN usuario
+			ON u.idUsuario = par.pkUsuario
+            JOIN sala s
+            ON par.pkSala = s.idSala group by s.idSala) ,'/',s.MaxJogadores) as MaximoJogadores,
+		   s.nome,
+           s.frequencia,
+           sis.nome,
+           s.visibilidade
+           FROM sala s
+           JOIN sistemas sis
+           ON s.fkSistema = sis.idSistemas
+           JOIN participacao par
+           ON s.idSala = par.pkSala
+           JOIN usuario u
+           ON par.pkUsuario = u.idUsuario
+           GROUP BY s.idSala ;
+           
+	-- LISTAGEM DE SALA PÚBLICA
+SELECT concat((select count(par.pkUsuario) FROM 
+             participacao JOIN usuario
+			ON u.idUsuario = par.pkUsuario
+            JOIN sala s
+            ON par.pkSala = s.idSala group by s.idSala) ,'/',s.MaxJogadores) as MaximoJogadores,
+		   s.nome,
+           s.frequencia,
+           sis.nome,
+           s.visibilidade
+           FROM sala s
+           JOIN sistemas sis
+           ON s.fkSistema = sis.idSistemas
+           JOIN participacao par
+           ON s.idSala = par.pkSala
+           JOIN usuario u
+           ON par.pkUsuario = u.idUsuario
+           GROUP BY s.idSala
+           having
+           s.visibilidade = 'Público';
+           
+	-- LISTAGEM DE SALA PRIVADA 
+    
+   SELECT concat((select count(par.pkUsuario) FROM 
+             participacao JOIN usuario
+			ON u.idUsuario = par.pkUsuario
+            JOIN sala s
+            ON par.pkSala = s.idSala group by s.idSala) ,'/',s.MaxJogadores) as MaximoJogadores,
+		   s.nome,
+           s.frequencia,
+           sis.nome,
+           s.visibilidade
+           FROM sala s
+           JOIN sistemas sis
+           ON s.fkSistema = sis.idSistemas
+           JOIN participacao par
+           ON s.idSala = par.pkSala
+           JOIN usuario u
+           ON par.pkUsuario = u.idUsuario
+           GROUP BY s.idSala
+           HAVING 
+           s.visibilidade = 'Privado';
+           
+           
+-- LISTAGEM DE SALAS QUE O USUARIO ESPECÍFCIO PARTICIPA
+    
+	SELECT concat(
+			 (select count(par.pkUsuario) FROM 
+             participacao JOIN usuario
+			ON u.idUsuario = par.pkUsuario
+            JOIN sala s
+            ON par.pkSala = s.idSala group by s.idSala) ,'/',s.MaxJogadores) as MaximoJogadores,
+            
+		   s.nome,
+           s.frequencia,
+           sis.nome,
+           s.visibilidade
+           FROM sala s
+           JOIN sistemas sis
+           ON s.fkSistema = sis.idSistemas
+           JOIN participacao par
+           ON s.idSala = par.pkSala
+           JOIN usuario u
+           ON par.pkUsuario = u.idUsuario
+           WHERE u.idUsuario = 1
+           GROUP BY s.idSala;
+           
+
+-- LISTAGEM DA QUANTIDADE DE JOGADORES POR SALA
+SELECT s.nome as NomeSala,
+	   count(par.pkUsuario) quantidadeJogadores
+       from 
+       sala s JOIN participacao par
+       ON par.pkSala = s.idSala 
+       group by s.idSala;
+
+END;
+        
+        
+		
+               
+               
+         
+BEGIN; -- SELECT DE PERSONAGENS DE UMA SALA ESPECÍFICA
+		SELECT s.idSala,
+			   s.nome,
+               u.nome as Usuario,
+			   per.nome as Personagem
+               FROM 
+               usuario u JOIN participacao p
+               ON u.idUsuario = p.pkUsuario
+               JOIN sala s
+               ON p.pkSala = s.idSala
+               JOIN sistemas sis
+               ON s.fkSistema = sis.idSistemas
+               JOIN personagem per
+               ON s.idSala = per.fkSala and per.fkUsuario = u.idUsuario 
+               WHERE s.idSala = 1;
+END;	
+
+    
+    SELECT 
+    personagem.nome
+    FROM sala JOIN personagem 
+    ON sala.idSala = personagem.fkSala 
+    WHERE idSala = 1;
